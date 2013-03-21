79d97d611c08e48457a2ea754079e41e97b84591
L-tan <dolciss@gmail.com>
2013-03-21 21:50:22 +0900

    Add animesp_check.php
    niconico anime special ( http://ch.nicovideo.jp/anime-sp )
    update check and add liveid to alert_live.php

---
+++ animesp_check.php
@@ -0,0 +1,72 @@
+<?php
+// animesp_check.php
+// ニコニコアニメスペシャルチェック
+
+require_once("Twitter.php");
+require_once("Nicovideo.php");
+require_once("alert_live.php");
+
+print("anime-sp check...");
+$url = "http://ch.nicovideo.jp/anime-sp";
+$data = file_get_contents($url);
+
+$add = "";
+$pattern = "/vid\" value=\"(\d+)\">.*?title\" value=\"(.*?)\">.*?open_time\" value=\"(\d+)\">/su";
+if(preg_match_all($pattern, $data, $match, PREG_SET_ORDER) > 0){
+	foreach($match as $item){
+		$liveid = "lv".$item[1];
+		$title = $item[2];
+		$opentime = $item[3];
+		if($opentime < (time() + 3600)){
+			// past...
+			continue;
+		}
+		if(in_array($liveid, $livelist)){
+			// already added
+			continue;
+		}
+		preg_match("/「(.*)」/u", $title, $match);
+		if(isset($match[1])){
+			$short = $match[1];
+		} else {
+			$short = $title;
+		}
+		$add .= "\t\t\t\"".$liveid."\", ";
+		$add .= "// ".date("m/d ", $opentime).$short.PHP_EOL;
+		$message = make_message("@L_tan ややっ、 ".$url." に",
+								$title,
+								date("(m/d H:i開場)", $opentime),
+								"が追加されたみたい？",
+								"http://nico.ms/".$liveid,
+								"");
+		statuses_update($mint_bot_m, $message);
+	}
+}
+if($add === ""){
+	print("new live not found...".PHP_EOL);
+	exit();
+}
+
+$fp = fopen("alert_live.php", "r+");
+if($fp){
+    flock($fp, LOCK_EX);
+    while($line = fgets($fp, 1024)){
+        // skip
+        if(strpos($line,"// animesp_check") !== false){
+            break;
+        }
+    }
+    if(!feof($fp)){
+        $pos = ftell($fp);
+        $remain = fread($fp, 1024*1024);
+        fseek($fp, $pos);
+        fwrite($fp, $add);
+        fwrite($fp, $remain);
+    } else {
+        print("// animesp_check not found...".PHP_EOL);
+    }
+    fclose($fp);
+}
+print("alert_live.php written.".PHP_EOL);
+
+?>
