d82354307c4c56439222b2b043223ba26d8e2fdb
L-tan <dolciss@gmail.com>
2013-07-25 23:55:36 +0900

    modify alert anime live

--- nico_alert.php
+++ nico_alert.php
@@ -391,7 +391,7 @@ function live_start($liveid, $commid, $userid)
 	}
 	if(($hit == "offi")
 		&&(strpos($ret, "電波諜報局") === FALSE)
-		&&(strpos($ret, "アニメ上映会") === FALSE)){
+		&&(strpos($ret, "ニコニコ生放送ではTVアニメ") === FALSE)){
 		print("lv".$liveid." Not Anime Official...".PHP_EOL);
 	} else {
 		if($hit == "offi"){
