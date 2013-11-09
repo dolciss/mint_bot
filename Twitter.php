6763f30123e6435887582d53dc3ee0b6358cf31f
L-tan <dolciss@gmail.com>
2013-11-09 21:48:58 +0900

    add kakkokariP

--- Twitter.php
+++ Twitter.php
@@ -452,6 +452,7 @@ function trim_name($screen_name, $name)
 					"kuhma_sohju" => "すみませんP（KuhmaSohju）",
 					"GOTS_FRUIT_P" => "フルーツ(笑)P",
 					"tomatoufu" => "トマ豆腐さん",
+					"kakkokari_" => "(仮)P",
 					);
 	if(isset($namelist[$screen_name])){
 		return $namelist[$screen_name];
